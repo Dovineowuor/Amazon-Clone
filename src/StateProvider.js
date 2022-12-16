60ab2499adcc545a2de74d0a72368c9bbea73c01
Dovine Owuor <owuordove@gmail.com>
2022-12-17 02:12:09 +0300

    Update StateProvider.js

--- src/StateProvider.js
+++ src/StateProvider.js
@@ -1,5 +1,6 @@
 import React, {createContext, useContext, useReducer} from 'react';
 
+<<<<<<< Updated upstream
 // Prepares the dataLayer
 export const StateContext = createContext();
 
@@ -7,9 +8,22 @@ export const StateContext = createContext();
 // Wrap our app and provide the Data Layer
 export const StateProvider = ({ reducer, initialState, children }) => (
     <StateContext.Provider value={useReducer(reducer,initialState)}>
+=======
+//Prepares the dataLayer
+export const StateContext = createContext();
+
+//Wrap our app and provide the dataLayer
+export const StateProvider = ({ reducer, initialState, children }) => (
+    <StateContext.Provider value={useReducer(reducer, initialState)}>
+>>>>>>> Stashed changes
         {children}
     </StateContext.Provider>
 );
 
+<<<<<<< Updated upstream
 // Pull information from the data LAyer
+=======
+
+//Pull info from the dateLayer
+>>>>>>> Stashed changes
 export const useStateValue = () => useContext(StateContext);
