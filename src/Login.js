55db507eff85eb8d2a0947a9f7c1c8b6aa207e65
Dovine Owuor <owuordove@gmail.com>
2022-12-17 02:10:48 +0300

    Update Login.js

--- src/Login.js
+++ src/Login.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React, { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
 import { auth } from "./firebase";
@@ -109,3 +110,54 @@ function Login() {
 }
 
 export default Login;
+=======
+import React, { useState } from 'react';
+import './Login.css'
+import { Link, useHistory } from 'react-router-dom';
+import { auth } from './firebase';
+
+function Login() {
+    const history = useHistory();
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const signIn = e => {
+        e.preventDefault();
+        //firebase login\
+        auth.signInWithEmailAndPassword(email, password).then(auth => {
+            history.push('/');
+        }).catch(error => alert(error.message));
+    }
+    const register = e => {
+        e.preventDefault();
+        // firebase register
+        auth.createUserWithEmailAndPassword(email, password).then((auth) => {
+            history.push('/');
+            //console.log(auth);
+        }).catch(error => alert(error.message));
+    }
+    return (
+        <div className="login">
+            <Link to='/'>
+              <img className='login__logo' src="https://upload.wikimedia.org/wikipedia/commons/thumb/a/a9/Amazon_logo.svg/1024px-Amazon_logo.svg.png" />
+            </Link>
+
+            <div className="login__container">
+                <h1>Sign-in</h1>
+                <form>
+                    <h5>E-mail</h5>
+                    <input type='text' value={email} onChange={e => setEmail(e.target.value)}/>
+                    <h5>Password</h5>
+                    <input type='password' value={password} onChange={e => setPassword(e.target.value)}/>
+                    <button onClick={signIn} type='submit' className="login__signInButton">Sign in</button>
+                </form>
+                <p>By signing-in you agree to the Amazon's Contitions of Use & Sale. Please see our Privacy Notice, our Cookie Notice and our Interest-Based Ads Notice</p>
+
+                <button onClick={register} className="login__registerButton">Create your Amazon Account</button>
+            </div>
+            
+        </div>
+    )
+}
+
+export default Login
+>>>>>>> Stashed changes
