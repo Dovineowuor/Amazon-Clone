aff410e2f78f251866fd8dfe8e10f96abf07fecf
Dovine Owuor <owuordove@gmail.com>
2022-12-17 02:09:55 +0300

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import "./App.css";
 import Header from "./Header";
 import Home from "./Home";
@@ -87,6 +88,83 @@ function App() {
         />
       </Routes>
     </div>
+=======
+import React, { useEffect } from 'react';
+import './App.css';
+import Header from './Header'
+import Home from './Home'
+import Checkout from './Checkout'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Login from './Login'
+import { auth } from './firebase';
+import { useStateValue } from './StateProvider';
+import Payment from './Payment';
+import {loadStripe} from '@stripe/stripe-js';
+import {Elements} from '@stripe/react-stripe-js';
+import Orders from './Orders'
+
+const promise = loadStripe('pk_test_51HeJNDK860DkztEzJf2UqGmoGNfg4sRh5fSP8OqrTU1hz9tPSsIHVjvDONFHfPrIxBHm1Pp0ANVqJrHnG1WxCu5k00Rh4sPENp')
+
+function App() {
+
+  const [{}, dispatch] = useStateValue();
+
+  useEffect(() => {
+    //run once when the app component loads
+    auth.onAuthStateChanged(authUser => {
+      console.log('THE USER IS >>>', authUser);
+
+      if(authUser){
+        //The user just logged in
+        dispatch({
+          type: 'SET_USER',
+          user: authUser
+        })
+
+      } else{
+        //User is logged out
+        dispatch({
+          type: 'SET_USER',
+          user: null
+        })
+      }
+    })
+  }, []);
+
+  return (
+    <Router>
+      <div className="app">
+        
+
+        <Switch>
+          <Route path="/login">      
+            <Login/>
+          </Route>
+          <Route path="/orders">
+            <Header />     
+            <Orders/>
+          </Route>
+          <Route path="/checkout">  
+            <Header />     
+            <Checkout />
+          </Route>
+          <Route path="/payment">  
+            <Header /> 
+            <Elements stripe= {promise}>
+               <Payment />
+            </Elements>                
+          </Route>
+
+          <Route path="/">
+            <Header />
+            <Home />  
+          </Route>
+        </Switch>        
+      </div>
+    </Router>
+
+    
+>>>>>>> Stashed changes
   );
 }
 
