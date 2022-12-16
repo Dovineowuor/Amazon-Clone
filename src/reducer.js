48cd3383c12a8205bce4a566e325c2f137f42608
Dovine Owuor <owuordove@gmail.com>
2022-12-17 02:10:06 +0300

    Update reducer.js

--- src/reducer.js
+++ src/reducer.js
@@ -1,4 +1,5 @@
 export const initialState = {
+<<<<<<< Updated upstream
   basket: [],
   user: null,
   userClientId: [],
@@ -54,3 +55,57 @@ const reducer = (state, action) => {
 };
 
 export default reducer;
+=======
+    basket: [],
+    user: null
+};
+
+export const getBasketTotal = (basket) => 
+    basket?.reduce((amount, item) => item.price + amount, 0);
+
+
+const reducer  = (state, action) => {
+    console.log(action);
+    switch(action.type){
+        case 'ADD_TO_BASKET':
+            return{
+                ...state, 
+                basket: [...state.basket, action.item]
+            }
+        
+        case 'EMPTY_BASKET':
+            return{
+                ...state,
+                basket: []
+            }
+        case 'REMOVE_FROM_BASKET':
+            const index = state.basket.findIndex(
+                (basketItem) => basketItem.id === action.id
+            );
+            let newBasket = [...state.basket];
+
+            if(index >= 0){
+                newBasket.splice(index, 1);
+            } else{
+                console.warn(
+                    `Cant remove product (id: ${action.id}) as it's not in basket`
+                )
+            }
+
+            return {
+                ...state,
+                basket: newBasket
+            }
+        
+        case "SET_USER":
+            return{
+                ...state,
+                user: action.user
+            }
+        default:
+            return state;
+    }
+}
+
+export default reducer;
+>>>>>>> Stashed changes
