a87c4b69167f0435d63e0c773f3083d67be99a58
Dovine Owuor <owuordove@gmail.com>
2022-12-17 02:12:56 +0300

    Update firebase.js

--- src/firebase.js
+++ src/firebase.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore/lite';
 import { getAuth } from "firebase/auth";
@@ -18,3 +19,24 @@ const firebaseConfig = {
   const auth = getAuth(app);
 
 export {db, auth}
+=======
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import firebase from 'firebase'
+const firebaseConfig = {
+  apiKey: "AIzaSyAWkZwOScfoezFEbhCoLY_5TwQjoDIqwk4",
+  authDomain: "challege-329f3.firebaseapp.com",
+  databaseURL: "https://challege-329f3.firebaseio.com",
+  projectId: "challege-329f3",
+  storageBucket: "challege-329f3.appspot.com",
+  messagingSenderId: "842104251510",
+  appId: "1:842104251510:web:4a384a0c568a05fc992b57",
+  measurementId: "G-DPJP6LF7GV"
+};
+ const firebaseApp = firebase.initializeApp(firebaseConfig);
+
+ const db = firebaseApp.firestore();
+
+ const auth = firebase.auth()
+
+ export { db, auth};
+>>>>>>> Stashed changes
