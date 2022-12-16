6aa07d4fcf3a24533bc08643841f58d39f78b2f6
Dovine Owuor <owuordove@gmail.com>
2022-12-17 02:12:36 +0300

    Update Checkout.js

--- src/Checkout.js
+++ src/Checkout.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import "./Checkout.css";
 import CheckoutProduct from "./CheckoutProduct";
@@ -37,6 +38,44 @@ function Checkout() {
       </div>
     </div>
   );
+=======
+import React from 'react';
+import './Checkout.css';
+import Subtotal from './Subtotal'
+import { useStateValue } from './StateProvider';
+import CheckoutProduct from './CheckoutProduct'
+
+function Checkout() {
+    const [{basket, user}, dispatch ] = useStateValue()
+    return (
+        <div className="checkout">
+            <div className="checkout__left">
+                <img className="checkout__ad" 
+                     src="https://images-na.ssl-images-amazon.com/images/G/02/UK_CCMP/TM/OCC_Amazon1._CB423492668_.jpg" alt="amazon_logo" />
+
+                <div>
+                    <h3>Hello, {user?.email}</h3>
+                    <h2 className="checkout__title">Your shopping Basket</h2>
+                    {basket.map(item => (
+                        <CheckoutProduct 
+                            id = {item.id}
+                            title = {item.title}
+                            image = {item.image}
+                            price = {item.price}
+                            rating = {item.rating}
+                        />
+                    ))}
+                </div>
+            </div>
+
+            <div className="checkout__right">
+                <Subtotal />
+                
+            </div>
+
+        </div>
+    )
+>>>>>>> Stashed changes
 }
 
 export default Checkout;
