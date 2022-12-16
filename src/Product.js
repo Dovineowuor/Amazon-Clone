8212a4293546921310b099c0cc8d18cab1c6ae03
Dovine Owuor <owuordove@gmail.com>
2022-12-17 02:13:43 +0300

    Update Product.js

--- src/Product.js
+++ src/Product.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 import React from "react";
 import "./Product.css";
 import { useStateValue } from "./StateProvider";
@@ -61,6 +62,48 @@ function Product({ id, title, image, price, rating, hideButton }) {
       )}
     </div>
   );
+=======
+import React from 'react';
+import './Product.css'
+import { useStateValue } from './StateProvider';
+
+function Product({ id, title, image, price, rating}) {
+    const [{basket}, dispatch] = useStateValue();
+
+    const addToBasket = () => {
+        //Dispatch the item into the dataLayer
+        dispatch({
+            type: 'ADD_TO_BASKET',
+            item: {
+                id: id,
+                title:title,
+                image: image,
+                price: price,
+                rating: rating
+            }
+        })
+    }
+    return (
+        <div className="product">
+            <div className="product__info">
+                <p>{title}</p>
+                <p className="product_price">
+                <small>$</small>
+                <strong>{price}</strong>
+                </p>
+                <div className="product__rating">
+                    {Array(rating).fill().map((_, i) => (
+                        <p>🌟</p>
+                    ))}
+                
+                </div>
+            </div>
+
+            <img src={image}/>
+            <button onClick={addToBasket}>Add to Basket</button>
+        </div>
+    )
+>>>>>>> Stashed changes
 }
 
 export default Product;
